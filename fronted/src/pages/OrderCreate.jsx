ed852c58d34a804c18b9ea985e7f4cbee0ee4f34
林泽鑫 <1046110267@qq.com>
2020-10-13 14:21:40 +0800

    committ

--- fronted/src/pages/OrderCreate.jsx
+++ fronted/src/pages/OrderCreate.jsx
@@ -1,7 +1,11 @@
+import { useRequest } from 'ahooks';
 import { Button } from 'antd';
 
 export default ()=>{
-  // const createOrder;
+  const createOrder = useRequest({
+    url:'123',
+    method:"post"
+  })
   return <div id="components-button-demo-basic">
       <Button type="primary">创建1个订单</Button>
       <Button>创建10个订单</Button>
